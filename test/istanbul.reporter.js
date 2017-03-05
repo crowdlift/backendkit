90d0c40aa7354457275ccdb371185cf71a3cfae1
Joe Johnston <joe@simple10.com>
2017-03-05 12:34:52 -0800

    Report code coverage + short summary

--- test/istanbul.reporter.js
+++ test/istanbul.reporter.js
@@ -1,21 +1,43 @@
-/* eslint-disable */
 // From https://github.com/rollup/rollup-starter-project/blob/master/test/istanbul.reporter.js
 
 const instanbul = require('istanbul');
-const MochaSpecReporter = require('mocha/lib/reporters/spec');
+const Base = require('mocha/lib/reporters/base');
+const Spec = require('mocha/lib/reporters/spec');
+const color = require('mocha/lib/reporters/base').color;
 
-module.exports = function (runner) {
+// Customize reporters as needed.
+// Text reporter generates CLI output table.
+const REPORTERS = ['html', 'json', 'text'];
+
+function Reporter(runner) {
   const collector = new instanbul.Collector();
   const reporter = new instanbul.Reporter();
-  reporter.addAll(['html', 'json', 'text-summary']);
-  new MochaSpecReporter(runner);
+  reporter.addAll(REPORTERS);
 
-  runner.on('end', function() {
+  // eslint prefers Spec.call vs 'new Spec' when not assigning to a var
+  Spec.call(Spec.prototype, runner);
+
+  let passes = 0;
+  let failures = 0;
+
+  runner.on('pass', () => (passes += 1));
+  runner.on('fail', () => (failures += 1));
+
+  runner.on('end', () => {
+    // eslint-disable-next-line no-underscore-dangle
     collector.add(global.__coverage__);
 
-    reporter.write(collector, true);
-    // reporter.write(collector, true, function() {
-    //   process.stdout.write("Coverage report generated\n");
-    // });
+    reporter.write(collector, true, () => {
+      const fmt =
+        color('checkmark', Base.symbols.ok) +
+        color('suite', ' %d  ') +
+        color('fail', Base.symbols.err) +
+        color('suite', ' %d  ');
+      // eslint-disable-next-line no-console
+      console.log(fmt, passes, failures);
+    });
   });
-};
+}
+
+
+module.exports = Reporter;
