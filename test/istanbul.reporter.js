3a68c209028bdbf671a381b96009eda23e92b9d7
Joe Johnston <joe@simple10.com>
2017-03-05 00:00:13 -0800

    Switch to html and text-summary istanbul reporters
    Text summary now outputs on CLI after mocha runs.

--- test/istanbul.reporter.js
+++ test/istanbul.reporter.js
@@ -7,14 +7,15 @@ const MochaSpecReporter = require('mocha/lib/reporters/spec');
 module.exports = function (runner) {
   const collector = new instanbul.Collector();
   const reporter = new instanbul.Reporter();
-  reporter.addAll(['lcov', 'json']);
+  reporter.addAll(['html', 'json', 'text-summary']);
   new MochaSpecReporter(runner);
 
   runner.on('end', function() {
     collector.add(global.__coverage__);
 
-    reporter.write(collector, true, function() {
-      process.stdout.write("Coverage report generated\n");
-    });
+    reporter.write(collector, true);
+    // reporter.write(collector, true, function() {
+    //   process.stdout.write("Coverage report generated\n");
+    // });
   });
 };
