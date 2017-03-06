142afe7f80a1be549440f8ae38f7ca5d7f6fdbbe
Joe Johnston <joe@simple10.com>
2017-03-05 19:19:46 -0800

    Refactor rollup config to be modular

--- rollup.config.js
+++ rollup.config.js
@@ -1,55 +1,11 @@
-/*
-See:
-https://github.com/rollup/rollup/wiki/Plugins
-https://github.com/rollup/rollup/wiki/JavaScript-API
-*/
-
-import babel from 'rollup-plugin-babel';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import inject from 'rollup-plugin-inject';
-import json from 'rollup-plugin-json';
-import yaml from 'rollup-plugin-yaml';
-import filesize from 'rollup-plugin-filesize';
+import rollupConfig from './lib/rollup/config';
 
 const pkg = require('./package.json');
 
-const external = Object.keys(pkg.dependencies);
-
-const config = {
-  format: 'cjs',
-  sourceMap: true,
-  plugins: [
-    nodeResolve({
-      jsnext: true,
-      main: true,
-      preferBuiltins: true,
-    }),
-    commonjs(),
-    // Not sure if this is needed with babel
-    inject({
-      Promise: 'es6-promise',
-      include: '**/*.js',
-      exclude: 'node_modules/**',
-    }),
-    json({
-      exclude: 'node_modules/**',
-    }),
-    yaml({
-      exclude: 'node_modules/**',
-    }),
-    // See https://github.com/rollup/rollup-plugin-babel
-    babel({
-      exclude: 'node_modules/**',
-      externalHelpers: false,
-      runtimeHelpers: true,
-    }),
-  ],
-  external,
-};
-
-if (!process.env.QUIET) {
-  config.plugins.push(filesize());
-}
+const config = rollupConfig({
+  profile: 'serverless',
+  quiet: process.env.QUIET,
+  pkg,
+});
 
 export default config;
